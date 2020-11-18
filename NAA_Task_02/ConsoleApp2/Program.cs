5bcb40161bfdb403a6c7b29f94fc1aa4791977c7
vvnaider <74681660+vvnaider@users.noreply.github.com>
2020-11-19 02:06:00 +0300

    Add files via upload

---
+++ NAA_Task_02/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    namespace NAA_Task_02
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                string exit;//переменная для чтения текста
+
+                Console.WriteLine("Программа будет выполняться до тех пор, пока не будет введено слово 'exit'");
+                do//цикл
+                {
+                    Console.WriteLine("- * - - - - - - - - - - - - - - - - - - - - - * - - - - - - - - - - - - - - - - - - - - - * - ");
+                    exit = Convert.ToString(Console.ReadLine());
+
+                } while (exit != "exit");//условие завершения - пользователь ввёл exit
+
+
+                Console.WriteLine("\n");
+                Console.WriteLine("- * - - - - - - - - - - - - - - - - - - - - - * - - - - - - - - - - - - - - - - - - - - - * - ");
+                Console.WriteLine(" Окончание программы...");
+                Console.WriteLine("- * - - - - - - - - - - - - - - - - - - - - - * - - - - - - - - - - - - - - - - - - - - - * -  ");
+
+            }
+        }
+    }
