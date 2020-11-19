9e9ff7b5344ba824b7312b3575a6a8256417da56
vvnaider <74681660+vvnaider@users.noreply.github.com>
2020-11-19 22:34:49 +0300

    Add files via upload

---
+++ NAA_Task_07/NAA_Task_07/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+
+namespace NAA_Task_07
+{
+    class Program
+    {
+        static void RandomFill(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = random.Next();
+            }
+        }
+
+        static void Print(int[] array)
+        {
+            for (int i = 0; i < array.Length; ++i)
+            {
+                Console.WriteLine(i + ": " + array[i]);
+            }
+        }
+
+        static int[] Shuffle(int[] array)
+        {
+            Random random = new Random();
+
+            int temporaryElement, randomPosition;
+
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                randomPosition = random.Next(i);
+                temporaryElement = array[i];
+                array[i] = array[randomPosition];
+                array[randomPosition] = temporaryElement;
+            }
+
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n        Введите размерность массива: ");
+            uint arraySize = Convert.ToUInt32(Console.ReadLine());
+
+            int[] array = new int[arraySize];
+            RandomFill(array);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\n        Вывод исходного массива: ");
+            Print(array);
+
+            Shuffle(array);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n        Вывод перемешанного массива: ");
+            Print(array);
+
+            Console.ReadKey();
+        }
+    }
+}
